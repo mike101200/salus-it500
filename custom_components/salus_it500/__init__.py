46a021655f485b4ed3e471a34369f7c4c3fe8f46
Mihai Antom <mike101200@outlook.com>
2026-01-30 11:19:57 +0200

    Increased version number

--- custom_components/salus_it500/__init__.py
+++ custom_components/salus_it500/__init__.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 import homeassistant.helpers.config_validation as cv
 
-__version__ = "0.0.9"
+__version__ = "0.1.0"
 
 DOMAIN = "salus_it500"
 PLATFORMS = ["climate", "text", "number"]
